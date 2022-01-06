e8e10c0f5384ad67bbb5030d43e05014e1d0f7cc
naseemzyngl <naseem@zartek.in>
2022-01-06 13:18:32 +0530

    Updates

---
+++ precache-manifest.23fb38aa33e3e63a2db02881f29cddfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ddd681c80197730ed401e8e511ed082",
+    "url": "/facebookpages/index.html"
+  },
+  {
+    "revision": "024e7541b914154ef7c6",
+    "url": "/facebookpages/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "481edcb264d3d8a61813",
+    "url": "/facebookpages/static/js/2.b9feba1f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/facebookpages/static/js/2.b9feba1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024e7541b914154ef7c6",
+    "url": "/facebookpages/static/js/main.0a768608.chunk.js"
+  },
+  {
+    "revision": "d62fc3612a044f478123",
+    "url": "/facebookpages/static/js/runtime-main.e164d125.js"
+  }
+]);
